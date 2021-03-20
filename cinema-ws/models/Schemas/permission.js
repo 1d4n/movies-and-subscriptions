094dd3e392be3affeaa3503818eb96c4b4f418c7
Idan <55255630+1d4n@users.noreply.github.com>
2021-03-20 16:13:43 +0200

    -

--- cinema-ws/models/Schemas/permission.js
+++
@@ -1,8 +0,0 @@
-const mognoose = require("mongoose");
-
-const Permission = new mognoose.Schema({
-  _id: mognoose.Types.ObjectId,
-  permissions: [{ type: String }],
-});
-
-module.exports = mognoose.model("Permission", Permission, "permissions");
